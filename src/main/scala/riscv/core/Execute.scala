00e7d08e5d13de85533b9702bcae41ffe2cba195
TRChen11011 <TRChen11011@gmail.com>
2023-11-20 22:48:10 +0800

    EXE,CPU ver2

--- src/main/scala/riscv/core/Execute.scala
+++ src/main/scala/riscv/core/Execute.scala
@@ -38,8 +38,12 @@ class Execute extends Module {
 
   // lab3(Execute) begin
   alu.io.func := alu_ctrl.io.alu_funct
-  alu.io.op1 := io.reg1_data
-  alu.io.op2 := io.reg2_data
+  alu.io.op1  := Mux(io.aluop1_source.asBool,
+                      io.instruction_address,
+                      io.reg1_data)
+  alu.io.op2  := Mux(io.aluop2_source.asBool,
+                      io.instruction_address,
+                      io.reg1_data)
   // lab3(Execute) end
 
   io.mem_alu_result := alu.io.result
